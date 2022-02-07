50c3f714e9ecd9fa4701120745affa309e6b5479
Laetitia Rodriguez <rodriguezl.laetitia@gmail.com>
2022-02-07 17:37:17 +0100

    challenge1

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+$name = 'Voyage avec un âne dans les Cévennes';
+$read = false;
+$releaseYear = 2017;
+$price = 5.99;
+
+echo "$name<br>";
+if ($read == true) {
+    echo "C'est un très bon livre.<br>";
+} else {
+    echo "Je ne l'ai pas lu.<br>";
+}
+echo "$releaseYear<br>";
+echo "$price<br>";
