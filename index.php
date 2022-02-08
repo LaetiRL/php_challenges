490ff66cb4555c759cc4cc6d249ea516cf593943
Laetitia Rodriguez <rodriguezl.laetitia@gmail.com>
2022-02-08 13:12:42 +0100

    challenge4

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+$movies = [
+    'L\'île au trésor' => ['Bobby Driscol', 'Robert Newton', 'Basil Sydney'],
+    'Dr. Jekyll et Mr. Hyde' => ['Michael Caine', 'Cheryl Ladd', 'Joss Ackland'],
+    'Antoinette dans les Cévennes' => ['Laure Calamy', 'Benjamin Lavernhe', 'Olivia Côte'],
+];
+foreach($movies as $key => $values) {
+    echo "Dans le film $key, les principaux acteurs sont:\n";
+    foreach($values as $actors) {
+        echo $actors . ", ";
+    }
+    echo "\n";
+} 
