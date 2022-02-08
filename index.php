7f0d31bc1b5bef1dc2f1347c563fb8a463ccdbdb
Laetitia Rodriguez <rodriguezl.laetitia@gmail.com>
2022-02-08 14:58:52 +0100

    challenge5 modified

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 $weapons = ['fists', 'whip', 'gun'];
 $opponentWeapon = $weapons[rand(0,2)];
 
-if ($opponentWeapon == $weapons[0]) {
+if ($opponentWeapon === $weapons[0]) {
     $stevensonWeapon = $weapons[2];
-    echo "L'opposant utilise $opponentWeapon, alors Stevenson utilise $stevensonWeapon.";
-} elseif ($opponentWeapon == $weapons[1]) {
+} elseif ($opponentWeapon === $weapons[1]) {
     $stevensonWeapon = $weapons[0];
-    echo "L'opposant utilise $opponentWeapon, alors Stevenson utilise $stevensonWeapon.";
 } else {
     $stevensonWeapon = $weapons[1];
-    echo "L'opposant utilise $opponentWeapon, alors Stevenson utilise $stevensonWeapon.";
 }
+echo "L'opposant utilise $opponentWeapon, alors Stevenson utilise $stevensonWeapon.";
