36389927b33b9c90089d902d0d0ac8e16a1d0985
Laetitia Rodriguez <rodriguezl.laetitia@gmail.com>
2022-02-11 14:25:50 +0100

    challenge8

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-<?php
+<form action="thanks.php" method="POST">
+    <div>
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="user_name">
+    </div>
+    <div>
+        <label for="fname">Prénom :</label>
+        <input type="text" id="fname" name="user_fname">
+    </div>
+    <div>
+        <label for="mail">E-mail :</label>
+        <input type="email" id="mail" name="user_mail">
+    </div>
+    <div>
+        <label for="phone">Téléphone :</label>
+        <input type="tel" id="phone" name="phone" pattern="[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}" placeholder="06XXXXXXXX">
+    </div>
+    <div>
+        <label for="subject">Sujet :</label>
+        <select id="subject" name="subject">
+            <option value="questions">Questions</option>
+            <option value="devis">Devis</option>
+            <option value="bugs">Bugs détectés</option>
+        </select>
+    </div>
+    <div>
+        <label for="msg">Message :</label>
+        <textarea id="msg" name="user_message"></textarea>
+    </div>
+    <div class="button">
+        <button type="submit">Envoyer le message</button>
+    </div>
+</form>
---
+++ thanks.php
@@ -0,0 +1,4 @@
+<?php
+echo "Merci ".$_POST["user_fname"]." ". $_POST["user_name"]." de nous avoir contacté à propos de ".$_POST["subject"].". ".
+"Un de nos conseiller vous contactera soit à l’adresse ".$_POST["user_mail"]. " ou par téléphone au ".$_POST["phone"]. " dans les plus brefs délais pour traiter votre demande : ".$_POST["user_message"];
+
