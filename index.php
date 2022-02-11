7cc0ca5c5c8eae27001f1a184fabec79003a3ff4
Laetitia Rodriguez <rodriguezl.laetitia@gmail.com>
2022-02-11 11:30:26 +0100

    challenge7

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Laetitia" => [
+        "city" => "Paris",
+        "weapon" => "compound bow"
+    ]
+];
+var_dump($characters);
